8048b3ba45ea4596184f74b5ab2276c5df9b7c4d
Kino  Rose <kinoarose@gmail.com>
2020-12-28 18:44:45 -0500

    Initial commit

---
+++ source/Globals.hx
@@ -0,0 +1,3 @@
+class Globals {
+	public static inline var COMMANDTIMESCALE = 0.015;
+}
---
+++ source/import.hx
@@ -0,0 +1,16 @@
+import Globals;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.FlxSubState;
+import flixel.group.FlxGroup;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.ui.FlxBar;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import flixel.util.FlxSave;
+
+using Lambda;
+using ext.MathExt;
+using flixel.util.FlxSpriteUtil;
